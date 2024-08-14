169f5d7720aad322575df179ee8fb5616e5db0a5
JackyWuCoder <JackyWuCoder@gmail.com>
2024-08-13 20:02:27 -0700

    Basic Methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10.0f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    private void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    private void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
